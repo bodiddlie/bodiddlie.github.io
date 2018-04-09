e9ae368a3cd3b1bd8161e9c626a033ce4988f0cb
bodiddlie <bodiddlie@gmail.com>
2018-04-08 17:57:45 -0700

    added analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,5 +29,11 @@ module.exports = {
       },
     },
     `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-73423130-1",
+      }
+    }
   ],
 };
